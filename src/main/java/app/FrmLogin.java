ded934821ebd003fe6bee3ba56c258d05a5dc558
Mark <jpmarkbusiness@gmail.com>
2022-05-13 15:49:24 -0500

    Version con Login

--- src/main/java/app/FrmLogin.java
+++ src/main/java/app/FrmLogin.java
@@ -14,6 +14,7 @@ import model.Usuario;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -22,12 +23,14 @@ import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
 
 public class FrmLogin extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtUsuario;
+
 	/**
 	 * Launch the application.
 	 */
@@ -63,32 +66,52 @@ public class FrmLogin extends JFrame {
 		});
 		btnNewButton.setBounds(324, 29, 89, 23);
 		contentPane.add(btnNewButton);
-		
+
 		txtUsuario = new JTextField();
 		txtUsuario.setBounds(122, 30, 161, 20);
 		contentPane.add(txtUsuario);
 		txtUsuario.setColumns(10);
-		
+
 		JLabel lblNewLabel = new JLabel("Usuario :");
 		lblNewLabel.setBounds(10, 33, 102, 14);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblClave = new JLabel("Clave :");
 		lblClave.setBounds(10, 64, 102, 14);
 		contentPane.add(lblClave);
-		
+
 		txtClave = new JTextField();
 		txtClave.setColumns(10);
 		txtClave.setBounds(122, 61, 161, 20);
 		contentPane.add(txtClave);
-		
+
 	}
 
-	
 	private JTextField txtClave;
-	
-	
+
 	void registrar() {
-		
+
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
+		EntityManager em = fabrica.createEntityManager();
+
+		Query consulta = em.createNativeQuery("{call usp_validaAcceso(:usuario,:clave)}", Usuario.class);
+		consulta.setParameter("usuario", txtUsuario.getText());
+		consulta.setParameter("clave", txtClave.getText());
+
+		Usuario u;
+		try {
+			u = (Usuario) consulta.getSingleResult();
+		} catch (Exception e) {
+			u = null;
+		}
+
+		if (u == null) {
+			JOptionPane.showMessageDialog(null, "Usuario o clave incorrecta");
+		} else {
+			JOptionPane.showMessageDialog(null, "Bienvenido...");
+		}
+
+		em.close();
+
 	}
 }
